d9a319d9c65e297540067333993ffb344336999a
Marcos Brandan <marcosbrandan681@gmail.com>
2023-05-21 11:22:11 -0300

    Se implemento el calculo del total a pagar con sus respectivos descuentos

--- newPage/script.js
+++ newPage/script.js
@@ -1,10 +1,29 @@
 function calcular(){
     var pago = document.getElementById('valor').value;
-    
-    pago = 200 * pago;
+    var categoria = document.getElementById('categoria').value;
+    var descuento;
 
+    switch(categoria){
+        case "1":
+            pago = 200 * pago;
+            descuento = pago * 0.80
+            pago -= descuento;
+            break;
+        case "2":
+            pago = 200 * pago;
+            descuento = pago * 0.50
+            pago -= descuento;
+            break;
+        case "3":
+            pago = 200 * pago;
+            descuento = pago * 0.15
+            pago -= descuento;
+            break;
+    }
     document.getElementById('total').innerHTML = "Total a Pagar: $" + pago;
 }
+
+
 
 const btnResumenClick = document.getElementById('botonResumen');
 btnResumenClick.addEventListener("click",calcular);
